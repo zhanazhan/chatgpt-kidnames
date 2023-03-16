ef4548a2f8e95119f2cf190a796f86c1ef644825
Zhandos Ospanov <zhanazhan@gmail.com>
2023-03-16 17:22:33 +0600

    add stop words.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -30,6 +30,7 @@ export default async function (req, res) {
       model: "text-davinci-003",
       prompt: generatePrompt(kiddoNationality),
       temperature: 0.6,
+      stop: ["Kid:", "Names:"],
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch(error) {
