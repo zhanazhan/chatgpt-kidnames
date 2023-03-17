a600d406ff834b9e2df9e7d14b8343748dd8f352
Zhandos Ospanov <zhanazhan@gmail.com>
2023-03-17 17:18:09 +0600

    add stop words.

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,44 @@ export default function Home() {
   const [changed, setChanged] = useState(true);
   const [result, setResult] = useState();
 
-  async function onSubmit(event) {
-    event.preventDefault();
+  const serverRequest= async (reqKidParam, callback) => {
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ kiddo: kiddoInput }),
+        body: JSON.stringify({ kiddo: reqKidParam }),
       });
 
       const data = await response.json();
       if (response.status !== 200) {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
-      setChanged(false);
-      setResult(data.result);
-      setKiddoInput("");
+      callback && await callback(data.result);
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
       alert(error.message);
     }
+  }
+
+  const retry = async () => {
+    setChanged(true);
+    await serverRequest(request, (data) => {
+      setChanged(false);
+      setResult(data);
+      setKiddoInput("");
+    });
+  }
+
+  const onSubmit = async (event) => {
+    event && event.preventDefault();
+    await serverRequest(kiddoInput, (data) => {
+      setChanged(false);
+      setResult(data);
+      setKiddoInput("");
+    });
   }
 
   return (
@@ -57,8 +72,10 @@ export default function Home() {
           />
           <input type="submit" value="Generate names" />
         </form>
+        {changed && kiddoInput && <div className={styles.request}>Loading my suggestions.</div>}
         {!changed && <div className={styles.request}>Trending name for a <span className={styles.requestMain}>{request}</span>:</div>}
         {!changed && <div className={styles.result}>{result}</div>}
+        {!changed && <button className={styles.reload} onClick={(e) => retry()}>Try again</button>}
       </main>
     </div>
   );
